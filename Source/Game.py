fcdf91eafdbe06c807a544de9b56a79200a47f7d
N.Duy_Computer <lnhatduy27@gmail.com>
2025-03-06 01:38:19 +0700

    update animation for button and make some logical

--- Source/Game.py
+++ Source/Game.py
@@ -3,24 +3,6 @@ import pygame.freetype
 import random
 from Context_Interactive import *
 
-import sys
-import os
-
-# Lấy đường dẫn thư mục hiện tại (Source/)
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Lấy đường dẫn thư mục cha (PacManProject/)
-parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
-
-# Thêm Const_definition vào sys.path
-sys.path.append(parent_dir)
-
-# Import Properties.py
-from Const_definition.Properties import *
-
-
-
-
 class GAME:
     
     def __init__(self):
@@ -31,27 +13,18 @@ class GAME:
         self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         self.title = pygame.display.set_caption(WINDOW_TITLE)
         
-        
-        
         self.home_background = pygame.image.load(HOME_BACKGROUND)
-        self.home_background = pygame.transform.scale(self.home_background, (HOME_WIDTH, HOME_HEIGHT))
-        
-        # Sao chép thông tin nút
-        self.button_original = BUTTON
-        self.button_scaled = BUTTON
-        self.button_rect = START_BUTTON.copy()
-
-        # Trạng thái hiệu ứng
-        self.button_hovered = False
+        self.home_background = pygame.transform.scale(self.home_background, (HOME_WIDTH, HOME_HEIGHT))   
     
-    
-    
+    # DISPLAY HOME SCREEN
     def Menu_draw(self):
-        """Vẽ màn hình trang chủ"""
+        
         self.screen.fill(BLACK)
         self.screen.blit(self.home_background, (0, 0))
+        
         self.screen.blit(BUTTON, START_BUTTON)
-        self.draw_button_text()
+        draw_button_with_text(self, BUTTON, START_BUTTON, "START")
+        
         pygame.display.update()  # Cập nhật màn hình
         
     
@@ -59,11 +32,16 @@ class GAME:
 
 
     def run_game(self):
-        """Chạy vòng lặp game"""
+        
         running = True
         while running:
             
-            self.button_animation()
+            mouse_pos = pygame.mouse.get_pos()
+            
+            global BUTTON, START_BUTTON, START_BUTTON_HOVERED
+            BUTTON, START_BUTTON, START_BUTTON_HOVERED = button_animation(
+                BUTTON_ORIGIN, BUTTON, START_BUTTON, START_BUTTON_HOVERED, mouse_pos
+            )
             self.Menu_draw()
 
             for event in pygame.event.get():
